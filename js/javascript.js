09ed8eb3905b4280c3f08be5e7a45fcb0cc6a2d2
John Kent <jkent_93@hotmail.com>
2012-10-07 13:44:46 +1000

    ive placed the javascript within the js folder
    i have also created alot of functionality to the graphs such as multiple
    countries selected, specific type of graphs (gdp,pop,medal) will be
    adding advanced manipulation to the graphs soon such as averages,
    comparisons, ratios

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,5 @@
+//top menu//
+////////////////////////////////////////////////////////////////////////
 var DDSPEED = 10;
 var DDTIMER = 15;
 
@@ -63,4 +65,163 @@ function d_country(){
   var id = document.getElementById("id")
   alert(id.innerHTML);
 
+}
+
+//Mashup code.//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+var current_code = ""
+
+var gdp_data = {
+  AU: [[2009, 40479], [2010, 41102], [2011, 41318], [2012, 60000]],
+  NZ: [[2009, 27192], [2010, 27617], [2011, 27700], [2012, 50000]],
+  RU: [[2009, 47192], [2010, 27617], [2011, 27700], [2012, 500300]],
+}
+
+var pop_data = {
+  AU: [[2009, 27192], [2010, 27623], [2011, 27700], [2012, 50000]],
+  RU: [[2009, 47192], [2010, 276137], [2011, 27700], [2012, 50000]],
+}
+
+var medal_data = {
+  AU: [[2009, 47192], [2010, 276223], [2011, 277200], [2012, 520000]],
+  RU: [[2009, 472192], [2010, 27617], [2011, 27700], [2012, 50000]],
+}
+
+var selected_Countries =  []
+
+$(document).ready(function() {
+  
+  var map = new jvm.WorldMap ({
+
+      container: $("div#worldmap"),  
+    map: 'world_mill_en',
+    onRegionOver: function(event, code){
+      //console.log('region-over', code, map.getRegionName(code));
+    },
+    onRegionClick: function(event, code) {
+
+      append_country(code);
+      checkAccordion(code);  
+    },
+  }); 
+
+  function append_country(code) {
+    var exists = false
+    if (selected_Countries.length != 0 ){
+      for (i=0;i<selected_Countries.length;i++){
+        if (code == selected_Countries[i]){
+          exists = true
+        }
+      };
+    }
+    else { 
+    };
+
+    if (exists == false){
+      selected_Countries.push(code);
+    }
+    console.log(selected_Countries);  
+  };
+});
+  var current_values = []
+  function plotwithoption(code) {
+    current_code = code
+    current_values = []
+    for (i=0;i<selected_Countries.length;i++){
+      $.each(gdp_data, function(key, value) {
+        if (key == selected_Countries[i]) {
+          key = value
+          current_values.push(key);
+        }
+      });
+    };
+    console.log('plot with option'+current_values)
+    $.plot($("#placeholder"), current_values);         
+  };
+
+  function plotafterintial(catagory){ 
+    current_values = []
+    for (i=0;i<selected_Countries.length;i++){
+      $.each(catagory, function(key,value){
+        if (key == selected_Countries[i]) {
+          key = value
+          current_values.push(key);
+        }
+      });
+    };
+    $.plot($("#placeholder"), current_values);         
+  };
+
+//accordion//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+var ContentHeight = 700;
+//content global height.
+var TimeToSlide = 250.0;
+//how fast the accordion opens and closes.
+
+var openAccordion = '';
+
+function checkAccordion(code){
+  if (code == current_code) {
+    runAccordion(1);
+    selected_Countries = [code];
+  }
+  plotwithoption(code);
+}
+
+function runAccordion(index)
+//runs the animate function also it creates a delay after pressing. it also updates which slide is open and supplies the previously opened slide as the closing id.
+{
+  var nID = "Accordion" + index + "Content";
+  if(openAccordion == nID)
+  nID = '';
+  
+  setTimeout("animate(" + new Date().getTime() + "," + TimeToSlide + ",'" 
+    + openAccordion + "','" + nID + "')", 100);
+    //delay set.
+  
+  openAccordion = nID;
+  //sets open accordion
+}
+function animate(lastTick, timeLeft, closingId, openingId)
+//animates the accordion opening/closing.
+{  
+  var curTick = new Date().getTime();
+  var elapsedTicks = curTick - lastTick;
+  
+  var opening = (openingId == '') ? null : document.getElementById(openingId);
+  var closing = (closingId == '') ? null : document.getElementById(closingId);
+ 
+  if(timeLeft <= elapsedTicks)
+  {
+  if(opening != null)
+  //opens the accordion
+    opening.style.height = ContentHeight + 'px';
+  
+  if(closing != null)
+  //hides the accordion
+  {
+    closing.style.display = 'none';
+    closing.style.height = '0px';
+  }
+  return;
+  }
+ 
+  timeLeft -= elapsedTicks;
+  var newClosedHeight = Math.round((timeLeft/TimeToSlide) * ContentHeight);
+
+  if(opening != null)
+  //displays the accordion that was pressed.
+  {
+  if(opening.style.display != 'block')
+    opening.style.display = 'block';
+  opening.style.height = (ContentHeight - newClosedHeight) + 'px';
+  }
+  
+  if(closing != null)
+  //closes the accordion.
+  closing.style.height = newClosedHeight + 'px';
+
+  setTimeout("animate(" + curTick + "," + timeLeft + ",'" 
+    + closingId + "','" + openingId + "')", 33);
 }
